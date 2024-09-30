c25210250d1dd337e27fb60af25548c41c9bcdad
Dheekshith <dheekshith1999@gmail.com>
2024-09-30 17:01:14 +0530

    auth token extra check for mobile

--- controllers/middlewaresControllers/createAuthMiddleware/isValidAuthToken.js
+++ controllers/middlewaresControllers/createAuthMiddleware/isValidAuthToken.js
@@ -1,52 +1,70 @@
 const jwt = require('jsonwebtoken');
-
 const mongoose = require('mongoose');
 
 const isValidAuthToken = async (req, res, next, { userModel, jwtSecret = 'JWT_SECRET' }) => {
   try {
     const UserPassword = mongoose.model(userModel + 'Password');
     const User = mongoose.model(userModel);
-    const token = req.cookies.token;
-    if (!token)
+    
+    // Check for token in cookies first
+    let token = req.cookies.token;
+
+    // If cookie token is not present, check for Bearer token in Authorization header
+    if (!token) {
+      const authHeader = req.headers.authorization;
+      if (authHeader && authHeader.startsWith('Bearer ')) {
+        token = authHeader.split(' ')[1]; // Extract the token from the Bearer string
+      }
+    }
+    
+    // If no token found in both places, deny access
+    if (!token) {
       return res.status(401).json({
         success: false,
         result: null,
         message: 'No authentication token, authorization denied.',
         jwtExpired: true,
       });
+    }
 
+    // Verify the token
     const verified = jwt.verify(token, process.env[jwtSecret]);
 
-    if (!verified)
+    if (!verified) {
       return res.status(401).json({
         success: false,
         result: null,
         message: 'Token verification failed, authorization denied.',
         jwtExpired: true,
       });
+    }
 
+    // Fetch user and userPassword from the database
     const userPasswordPromise = UserPassword.findOne({ user: verified.id, removed: false });
     const userPromise = User.findOne({ _id: verified.id, removed: false });
 
     const [user, userPassword] = await Promise.all([userPromise, userPasswordPromise]);
 
-    if (!user)
+    // Check if user exists
+    if (!user) {
       return res.status(401).json({
         success: false,
         result: null,
-        message: "User doens't Exist, authorization denied.",
+        message: "User doesn't exist, authorization denied.",
         jwtExpired: true,
       });
+    }
 
+    // Check if the token is part of the user's logged sessions
     const { loggedSessions } = userPassword;
-    if (!loggedSessions.includes(token))
+    if (!loggedSessions.includes(token)) {
       return res.status(401).json({
         success: false,
         result: null,
-        message: 'User is already logout try to login, authorization denied.',
+        message: 'User is already logged out, try to login, authorization denied.',
         jwtExpired: true,
       });
-    else {
+    } else {
       const reqUserName = userModel.toLowerCase();
       req[reqUserName] = user;
       next();
